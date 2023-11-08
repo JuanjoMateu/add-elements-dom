9819c8cfad72d079e52201a736f4675198ec8b33
JuanjoMateu <juanjo_mateu@hotmail.es>
2023-11-08 08:16:16 +0100

    Falta poner las css a ver si me da tiempo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
  // Aquí tu código
+
+const btnAgregarElemento = document.getElementById('agregar');
+const listaElementos = document.getElementById('lista');
+
+    btnAgregarElemento.addEventListener('click', function() {
+    const nuevoElemento = prompt('Ingrese un elemento:');
+
+        if (nuevoElemento !== null && nuevoElemento.trim() !== '') {
+            const elementoLista = document.createElement('li');
+            const textoElemento = document.createTextNode(nuevoElemento);
+            elementoLista.appendChild(textoElemento);
+            listaElementos.appendChild(elementoLista);
+            }
+        });
+
